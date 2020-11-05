44b1c5e3ac3aa5573c58a0a880e4ed1c046b6620
huseyinkus <kusshuso@gmail.com>
2020-11-05 16:39:27 +0300

    Update iterative-fac.py

--- odev-1/iterative-fac.py
+++ odev-1/iterative-fac.py
@@ -1,7 +1,12 @@
 sayi=int(input("Faktöriyeli alınacak sayıyı giriniz: "))
 carpim=1
 
-for i in range(sayi):
+if sayi < 0:  
+   print("Negatif sayıların faktöriyeli yoktur.")  
+elif sayi == 0:  
+   print("0 ! =  1")  
+else:  
+   for i in range(sayi):
     carpim*=i+1
 
 print(sayi,"! = ",carpim)
